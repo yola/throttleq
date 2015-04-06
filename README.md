8a30a6cf7db0168b9868855c75d6d9fca700c0e2
kahnjw <jarrod.kahn@gmail.com>
2015-04-06 10:55:08 -0700

    Add initial application code and tests

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # throttleq
-Limit the number of promises that can be in flight simultaneously
+
+Limit the number of promises that can be simultaneously in flight.
+
+## Usage
+
+```javascript
+var q = require('q');
+var throttle = require('throttleq');
+
+var promiseFactory = function() {
+  ...
+  return promise;
+};
+
+/* Takes promise returning function and number of simultaneously running
+ * promises.
+ */
+var promiseThrottle = throttle(promiseFactory, 4);
+
+q.all([
+  promiseThrottle(args),
+  ...
+  promiseThrottle(args)
+]);
+
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+var q = require('q');
+
+
+var throttle = function(promiseFactory, limit) {
+  var running = 0;
+  var semaphore;
+
+  var scheduleNextJob = function() {
+    if (running < limit) {
+      running++;
+      return q();
+    }
+
+    if(!semaphore) {
+      semaphore = q.defer();
+    }
+
+    return semaphore.promise.then(scheduleNextJob);
+  };
+
+  var processScheduledJobs = function() {
+    running--;
+
+    if (semaphore && running < limit) {
+      semaphore.resolve();
+      semaphore = null;
+    }
+  };
+
+  return function() {
+    var args = arguments;
+
+    var runJob = function() {
+      return promiseFactory.apply(this, args);
+    };
+
+    return scheduleNextJob()
+      .then(runJob.bind(this))
+      .then(processScheduledJobs);
+  };
+};
+
+
+module.exports = throttle;
